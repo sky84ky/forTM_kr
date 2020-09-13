bc4d7dbf8bf489153f35bbc78646bcad2006e66a
chanhojung <jungch@naver.com>
2020-09-13 10:04:54 +0900

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -220,23 +220,23 @@ class PathPlanner():
     org_angle_steers_des = self.angle_steers_des_mpc
 
     # atom
-    if steeringPressed:
-      delta_steer = self.angle_steers_des_mpc - angle_steers
-      xp = [-450,0,450]
-      fp2 = [5,0,5]
-      limit_steers = interp( steeringTorque, xp, fp2 )
-      if steeringTorque < 0:  # right
-        if delta_steer > 0:
-          self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, limit_steers, angle_steers )
-      elif steeringTorque > 0:  # left
-        if delta_steer < 0:
-          self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, limit_steers, angle_steers )
+    # if steeringPressed:
+    #   delta_steer = self.angle_steers_des_mpc - angle_steers
+    #   xp = [-450,0,450]
+    #   fp2 = [5,0,5]
+    #   limit_steers = interp( steeringTorque, xp, fp2 )
+    #   if steeringTorque < 0:  # right
+    #     if delta_steer > 0:
+    #       self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, limit_steers, angle_steers )
+    #   elif steeringTorque > 0:  # left
+    #     if delta_steer < 0:
+    #       self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, limit_steers, angle_steers )
 
-    elif v_ego_kph < 30:  # 30
-        xp = [5,15,30]
-        fp2 = [1,3,5]
-        limit_steers = interp( v_ego_kph, xp, fp2 )
-        self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, limit_steers, angle_steers )
+    # elif v_ego_kph < 30:  # 30
+    #     xp = [5,15,30]
+    #     fp2 = [1,3,5]
+    #     limit_steers = interp( v_ego_kph, xp, fp2 )
+    #     self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, limit_steers, angle_steers )
 
     #  Check for infeasable MPC solution
     mpc_nans = any(math.isnan(x) for x in self.mpc_solution[0].delta)
