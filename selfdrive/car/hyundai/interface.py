88a8bc6df54d2de2131e03d238c90943fe9972fa
wikarous <67613740+wikarous@users.noreply.github.com>
2020-09-25 09:49:19 +0900

    Update interface.py
    eq900l 세팅

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -137,16 +137,16 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.init('lqr')
 
     ret.lateralTuning.lqr.scaleBP = [20. * CV.KPH_TO_MS, 50. * CV.KPH_TO_MS]
-    ret.lateralTuning.lqr.scaleV = [1700.0, 1500.0]
+    ret.lateralTuning.lqr.scaleV = [1800.0, 1580.0]
 
-    ret.lateralTuning.lqr.ki = 0.009
+    ret.lateralTuning.lqr.ki = 0.015
 
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
     ret.lateralTuning.lqr.c = [1., 0.]
-    ret.lateralTuning.lqr.k = [-103.976, 450.]
-    ret.lateralTuning.lqr.l = [0.22, 0.318]
-    ret.lateralTuning.lqr.dcGain = 0.003
+    ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
+    ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
+    ret.lateralTuning.lqr.dcGain = 0.00285 #0.002237852961363602
 
     ret.steerRatio = 13.0
     ret.steerActuatorDelay = 0.2
@@ -155,7 +155,7 @@ class CarInterface(CarInterfaceBase):
     ret.steerRateCost = 0.55
 
     ret.steerMaxBP = [30. * CV.KPH_TO_MS, 60. * CV.KPH_TO_MS]
-    ret.steerMaxV = [1.5, 2.5]
+    ret.steerMaxV = [1.1, 2.5]
 
 
 
