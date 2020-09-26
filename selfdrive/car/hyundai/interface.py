ca5745658117b455d90148febd51d102a89106af
wikarous <67613740+wikarous@users.noreply.github.com>
2020-09-26 09:32:00 +0900

    Update interface.py
    Dcgain, Ki 값 수정(바르게님 참조함)

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -139,14 +139,14 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.lqr.scaleBP = [20. * CV.KPH_TO_MS, 50. * CV.KPH_TO_MS]
     ret.lateralTuning.lqr.scaleV = [1800.0, 1580.0]
 
-    ret.lateralTuning.lqr.ki = 0.015
+    ret.lateralTuning.lqr.ki = 0.01
 
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
     ret.lateralTuning.lqr.c = [1., 0.]
     ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
     ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-    ret.lateralTuning.lqr.dcGain = 0.00285 #0.002237852961363602
+    ret.lateralTuning.lqr.dcGain = 0.00278 #0.002237852961363602
 
     ret.steerRatio = 13.0
     ret.steerActuatorDelay = 0.2
