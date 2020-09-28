3f4070630315913452b64564fdc9ff14a8006461
wikarous <67613740+wikarous@users.noreply.github.com>
2020-09-28 19:42:28 +0900

    Update paint.cc
    멘트변경

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -625,7 +625,7 @@ static void bb_ui_draw_measures_left(UIState *s, int bb_x, int bb_y, int bb_w ) 
       snprintf(val_str, sizeof(val_str), "%.1f°",(scene->angleSteers));
 
       snprintf(uom_str, sizeof(uom_str), "");
-    bb_h +=bb_ui_draw_measure(s,  val_str, uom_str, "조향각",
+    bb_h +=bb_ui_draw_measure(s,  val_str, uom_str, "현재조향각",
         bb_rx, bb_ry, bb_uom_dx,
         val_color, lab_color, uom_color,
         value_fontSize, label_fontSize, uom_fontSize );
