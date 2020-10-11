73621f5f2ccbc1b4138a58a20dcbb76bea1849b8
stonerains <72440538+stonerains@users.noreply.github.com>
2020-10-11 21:22:14 +0900

    Update pathplanner.py
    학습

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -140,7 +140,7 @@ class PathPlanner():
     x = max(sm['liveParameters'].stiffnessFactor, 0.1)
     sr = max(sm['liveParameters'].steerRatio, 0.1)
     #VM.update_params(0.8, CP.steerRatio) # 타이어 강성계수 고정,조향비율 고정 계산
-    VM.update_params(0.8, sr)
+    VM.update_params(x, sr)
 
     curvature_factor = VM.curvature_factor(v_ego)
 
