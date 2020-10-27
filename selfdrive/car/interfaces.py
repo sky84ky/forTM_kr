d0ffc308990082cb9fdc4479e024bdb227c81944
stonerains <72440538+stonerains@users.noreply.github.com>
2020-10-27 15:50:56 +0900

    Update interfaces.py
    경고 몇개 지움

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -88,14 +88,14 @@ class CarInterfaceBase():
   def create_common_events(self, cs_out, extra_gears=[], gas_resume_speed=-1, pcm_enable=True):  # pylint: disable=dangerous-default-value
     events = Events()
 
-    if cs_out.doorOpen:
-      events.add(EventName.doorOpen)
-    if cs_out.seatbeltUnlatched:
-      events.add(EventName.seatbeltNotLatched)
+#    if cs_out.doorOpen:
+#      events.add(EventName.doorOpen)
+#    if cs_out.seatbeltUnlatched:
+#      events.add(EventName.seatbeltNotLatched)
     if cs_out.gearShifter != GearShifter.drive and cs_out.gearShifter not in extra_gears:
       events.add(EventName.wrongGear)
-    if cs_out.gearShifter == GearShifter.reverse:
-      events.add(EventName.reverseGear)
+#    if cs_out.gearShifter == GearShifter.reverse:
+#      events.add(EventName.reverseGear)
     if not cs_out.cruiseState.available:
       events.add(EventName.wrongCarMode)
     if cs_out.espDisabled:
