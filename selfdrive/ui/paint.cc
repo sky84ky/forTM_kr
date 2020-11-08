109699976c99164e1fc724ab69433e6ed0d8cd0e
durango-op <71580184+durango-op@users.noreply.github.com>
2020-11-08 23:37:22 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -560,10 +560,10 @@ static void ui_draw_debug(UIState *s)
       ui_print(s, ui_viz_rx, ui_viz_ry+450, "LaC:LQR");
     }
     nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
-    ui_print(s, ui_viz_rx_center, ui_viz_ry+650, "curvature");
+    ui_print(s, ui_viz_rx_center, ui_viz_ry+650, "커브");
     ui_print(s, ui_viz_rx_center, ui_viz_ry+700, "%.4f", scene.curvature);
-    ui_print(s, ui_viz_rx_center, ui_viz_ry+750, " LeftPoly(%%)    LaneWidth    RightPoly(%%)");
-    ui_print(s, ui_viz_rx_center, ui_viz_ry+800, "%4.1f                    %4.2f                    %4.1f", (scene.pathPlan.lPoly/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100, scene.pathPlan.laneWidth, (abs(scene.pathPlan.rPoly)/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100);
+    ui_print(s, ui_viz_rx_center, ui_viz_ry+750, " 좌측간격(m)    차선폭(m)    우측간격(m)");
+    ui_print(s, ui_viz_rx_center, ui_viz_ry+800, "%.2f                    %.2f                    %.2f", (scene.pathPlan.lPoly, scene.pathPlan.laneWidth, scene.pathPlan.rPoly);
   }
 }
 
