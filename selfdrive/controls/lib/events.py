77dc687fa2a417cda1c499b85cbbf37841f9688d
stonerains <72440538+stonerains@users.noreply.github.com>
2020-11-12 09:47:25 +0900

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -549,7 +549,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
   },
 
   EventName.pcmDisable: {
-    ET.USER_DISABLE: EngagementAlert(AudibleAlert.chimeDisengage),
+   # ET.USER_DISABLE: EngagementAlert(AudibleAlert.chimeDisengage),
   },
 
   EventName.buttonCancel: {
