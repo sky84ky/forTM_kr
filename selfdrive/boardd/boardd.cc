56908d1faaf2761f256d70002a48998a0e11b651
Vehicle Researcher <>
2020-11-13 12:16:00 +0900

    openpilot v0.8.0 release

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -43,7 +43,6 @@ volatile sig_atomic_t do_exit = 0;
 bool spoofing_started = false;
 bool fake_send = false;
 bool connected_once = false;
-bool ignition = false;
 
 struct tm get_time(){
   time_t rawtime;
@@ -106,8 +105,6 @@ void safety_setter_thread() {
   capnp::FlatArrayMessageReader cmsg(amsg);
   cereal::CarParams::Reader car_params = cmsg.getRoot<cereal::CarParams>();
   cereal::CarParams::SafetyModel safety_model = car_params.getSafetyModel();
-
-  panda->set_unsafe_mode(0);  // see safety_declarations.h for allowed values
 
   auto safety_param = car_params.getSafetyParam();
   LOGW("setting safety model: %d with param %d", (int)safety_model, safety_param);
@@ -195,8 +192,10 @@ void can_recv(PubMaster &pm) {
   // create message
   MessageBuilder msg;
   auto event = msg.initEvent();
-  panda->can_receive(event);
-  pm.send("can", msg);
+  int recv = panda->can_receive(event);
+  if (recv){
+    pm.send("can", msg);
+  }
 }
 
 void can_send_thread() {
@@ -257,9 +256,7 @@ void can_recv_thread() {
       useconds_t sleep = remaining / 1000;
       usleep(sleep);
     } else {
-      if (ignition){
-        LOGW("missed cycles (%d) %lld", (int)-1*remaining/dt, remaining);
-      }
+      LOGW("missed cycles (%d) %lld", (int)-1*remaining/dt, remaining);
       next_frame_time = cur_time;
     }
 
@@ -301,7 +298,7 @@ void can_health_thread() {
       panda->set_safety_model(cereal::CarParams::SafetyModel::NO_OUTPUT);
     }
 
-    ignition = ((health.ignition_line != 0) || (health.ignition_can != 0));
+    bool ignition = ((health.ignition_line != 0) || (health.ignition_can != 0));
 
     if (ignition) {
       no_ignition_cnt = 0;
@@ -390,6 +387,9 @@ void hardware_control_thread() {
   LOGD("start hardware control thread");
   SubMaster sm({"thermal", "frontFrame"});
 
+  // Other pandas don't have hardware to control
+  if (panda->hw_type != cereal::HealthData::HwType::UNO && panda->hw_type != cereal::HealthData::HwType::DOS) return;
+
   uint64_t last_front_frame_t = 0;
   uint16_t prev_fan_speed = 999;
   uint16_t ir_pwr = 0;
@@ -403,8 +403,15 @@ void hardware_control_thread() {
     cnt++;
     sm.update(1000); // TODO: what happens if EINTR is sent while in sm.update?
 
-#ifdef QCOM
     if (sm.updated("thermal")){
+      // Fan speed
+      uint16_t fan_speed = sm["thermal"].getThermal().getFanSpeed();
+      if (fan_speed != prev_fan_speed || cnt % 100 == 0){
+        panda->set_fan_speed(fan_speed);
+        prev_fan_speed = fan_speed;
+      }
+
+#ifdef QCOM
       // Charging mode
       bool charging_disabled = sm["thermal"].getThermal().getChargingDisabled();
       if (charging_disabled != prev_charging_disabled){
@@ -417,18 +424,7 @@ void hardware_control_thread() {
         }
         prev_charging_disabled = charging_disabled;
       }
-    }
 #endif
-
-    // Other pandas don't have fan/IR to control
-    if (panda->hw_type != cereal::HealthData::HwType::UNO && panda->hw_type != cereal::HealthData::HwType::DOS) continue;
-    if (sm.updated("thermal")){
-      // Fan speed
-      uint16_t fan_speed = sm["thermal"].getThermal().getFanSpeed();
-      if (fan_speed != prev_fan_speed || cnt % 100 == 0){
-        panda->set_fan_speed(fan_speed);
-        prev_fan_speed = fan_speed;
-      }
     }
     if (sm.updated("frontFrame")){
       auto event = sm["frontFrame"];
