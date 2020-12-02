df2f2633d1eb96d5fcf1ad19fdd2f971a0979d07
stonerains <72440538+stonerains@users.noreply.github.com>
2020-12-02 18:06:05 +0900

    Update carcontroller.py
    커브감속 수정

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -91,7 +91,7 @@ class CarController():
       self.mdps11_stat_last = 0
       self.spas_always = False
 
-    self.scc_smoother = SccSmoother(accel_gain=1.0, decel_gain=1.0, curvature_gain=0.8)
+    self.scc_smoother = SccSmoother(accel_gain=1.0, decel_gain=1.0, curvature_gain=0.87)
 
   def update(self, enabled, CS, frame, CC, actuators, pcm_cancel_cmd, visual_alert,
              left_lane, right_lane, left_lane_depart, right_lane_depart, set_speed, lead_visible, sm):
