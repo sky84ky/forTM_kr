654fb24e7850f725bcea18e1ea3579cc51bda34b
stonerains <72440538+stonerains@users.noreply.github.com>
2020-12-03 18:13:37 +0900

    Update scc_smoother.py
    수정

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -253,14 +253,14 @@ class SccSmoother:
         override_acc = acc
         accel = (op_accel + acc) / 2.
       else:        
-        accel = op_accel * interp(clu11_speed, [0., 25., 50., 51., 60., 100.], [2.4, 2.85, 1.7, 1.65, 1.4, 1.0])
+        accel = op_accel * interp(clu11_speed, [0., 25., 50., 51., 60., 100.], [2.4, 2.88, 1.7, 1.65, 1.4, 1.0])
 #        if 35 > lead.dRel > 15:
 #          if clu11_speed < 50:
 #            accel = op_accel * 2.5               
 #        else:
 #          accel = op_accel * interp(clu11_speed, [50., 60., 100.], [1.75, 1.4, 1.0])
     if accel > 0.:
-      accel *= self.accel_gain * interp(clu11_speed, [35., 100.], [1.5, 1.2])
+      accel *= self.accel_gain * interp(clu11_speed, [35., 70., 100.], [1.5, 1.25, 1.2])
     else:
       accel *= self.decel_gain * interp(clu11_speed, [70., 75.], [1.7928, 1.8])
 
