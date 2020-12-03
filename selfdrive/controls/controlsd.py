bc02529947a8a76b1c32e9c4d919c960a4ffbb7e
stonerains <72440538+stonerains@users.noreply.github.com>
2020-12-03 10:03:23 +0900

    Add files via upload
    네오키 수정

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -478,7 +478,7 @@ class Controls:
 
     if not self.read_only:
       # send car controls over can
-      can_sends = self.CI.apply(CC, self.sm)
+      can_sends = self.CI.apply(CC, self)
       self.pm.send('sendcan', can_list_to_can_capnp(can_sends, msgtype='sendcan', valid=CS.canValid))
 
     force_decel = (self.sm['dMonitoringState'].awarenessStatus < 0.) or \
