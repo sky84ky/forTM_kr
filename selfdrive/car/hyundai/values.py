0f64040e1056828b9b820eec3fd8ef676e0f4ca7
stonerains <72440538+stonerains@users.noreply.github.com>
2020-12-09 12:00:22 +0900

    Update values.py
    코나 hev 수정

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -433,21 +433,21 @@ FEATURES = {
   # Use TCU Message for Gear Selection
   "use_tcu_gears": {CAR.K5, CAR.SONATA19, CAR.VELOSTER, CAR.SONATA_LF_TURBO},
   # Use E_GEAR Message for Gear Selection
-  "use_elect_gears": {CAR.K5_HEV, CAR.IONIQ_EV_LTD, CAR.KONA_EV, CAR.SONATA_HEV, CAR.NIRO_EV, CAR.K7_HEV,
+  "use_elect_gears": {CAR.K5_HEV, CAR.IONIQ_EV_LTD, CAR.KONA_EV, CAR.SONATA_HEV, CAR.KONA_HEV, CAR.NIRO_EV, CAR.K7_HEV,
                       CAR.GRANDEUR_HEV},
   # Use E_EMS11 Message for Gas and Brake for Hybrid/ELectric
-  "use_elect_ems": {CAR.K5_HEV, CAR.IONIQ_EV_LTD, CAR.KONA_EV, CAR.SONATA_HEV, CAR.NIRO_EV, CAR.K7_HEV,
+  "use_elect_ems": {CAR.K5_HEV, CAR.IONIQ_EV_LTD, CAR.KONA_EV, CAR.SONATA_HEV, CAR.KONA_HEV, CAR.NIRO_EV, CAR.K7_HEV,
                     CAR.GRANDEUR_HEV},
   # send LFA MFA message for new HKG models
-  "send_lfa_mfa": {CAR.SONATA, CAR.PALISADE, CAR.SONATA_HEV, CAR.SANTA_FE, CAR.NIRO_EV},
+  "send_lfa_mfa": {CAR.SONATA, CAR.KONA_HEV, CAR.PALISADE, CAR.SONATA_HEV, CAR.SANTA_FE, CAR.NIRO_EV},
   "has_scc13": {},
   "has_scc14": {},
   # these cars use the FCA11 message for the AEB and FCW signals, all others use SCC12
-  "use_fca": {CAR.SONATA, CAR.ELANTRA, CAR.ELANTRA_GT_I30, CAR.STINGER, CAR.IONIQ, CAR.KONA, CAR.KONA_EV, CAR.FORTE,
+  "use_fca": {CAR.SONATA, CAR.ELANTRA, CAR.ELANTRA_GT_I30, CAR.STINGER, CAR.IONIQ, CAR.KONA, CAR.KONA_EV, CAR.KONA_HEV, CAR.FORTE,
               CAR.PALISADE, CAR.GENESIS_G70},
 
   "use_bsm": {CAR.SONATA, CAR.PALISADE, CAR.GENESIS, CAR.GENESIS_G70, CAR.GENESIS_G80, CAR.GENESIS_G90,
-              CAR.GENESIS_G90_L, CAR.KONA},
+              CAR.GENESIS_G90_L, CAR.KONA_HEV, CAR.KONA},
 
   "use_blinker_flash": {CAR.SONATA_LF_TURBO},
 
