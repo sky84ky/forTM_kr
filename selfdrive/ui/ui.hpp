79bc21b76ce98a1e9d2c68866451ee51363720ed
stonerains <72440538+stonerains@users.noreply.github.com>
2020-12-24 11:05:13 +0900

    Update ui.hpp
    색상 추가

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -56,6 +56,17 @@
 #define COLOR_WHITE_ALPHA(x) nvgRGBA(255, 255, 255, x)
 #define COLOR_YELLOW nvgRGBA(218, 202, 37, 255)
 #define COLOR_RED nvgRGBA(201, 34, 49, 255)
+#define COLOR_RED_ALPHA(x) nvgRGBA(201, 34, 49, x)
+#define COLOR_OCHRE nvgRGBA(218, 111, 37, 255)
+#define COLOR_OCHRE_ALPHA(x) nvgRGBA(218, 111, 37, x)
+#define COLOR_GREEN nvgRGBA(0, 255, 0, 255)
+#define COLOR_GREEN_ALPHA(x) nvgRGBA(0, 255, 0, x)
+#define COLOR_ORANGE nvgRGBA(255, 175, 3, 255)
+#define COLOR_ORANGE_ALPHA(x) nvgRGBA(255, 175, 3, x)
+#define COLOR_LIME nvgRGBA(0, 255, 0, 255)
+#define COLOR_LIME_ALPHA(x) nvgRGBA(0, 255, 0, x)
+#define COLOR_FORIP nvgRGBA(231, 255, 51, 255)
+#define COLOR_FORIP_ALPHA(x) nvgRGBA(231, 255, 51, x)
 
 #define UI_BUF_COUNT 4
 
