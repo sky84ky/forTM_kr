5bf7674ed78bd4b18790b1c32702446f3f6a5f8a
stonerains <indawoom@naver.com>
2021-03-17 14:11:10 +0900

    튜닝 업데이트

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -307,18 +307,21 @@ class SccSmoother:
 
       # Tuned by stonerains
 
-      if 0. < d < -lead.vRel * (8. + cruise_gap) * 2. and lead.vRel < -1.:
-        t = d / lead.vRel
-        acc = -(lead.vRel / t) * CV.MS_TO_KPH * 1.85
+      if 0. < d < -lead.vRel * (7.687 + cruise_gap) * 2. and lead.vRel < -1.:
+        t = d / lead.vRel * 0.978
+        acc = -(lead.vRel / t) * CV.MS_TO_KPH * 1.8
         override_acc = acc
         accel = (op_accel + acc) / 2.
       else:        
-        accel = op_accel * interp(clu11_speed, [0., 30., 38., 50., 51., 60., 100.], [2.3, 3.4, 3.2, 1.7, 1.65, 1.4, 1.0])
+        if 40 > lead.dRel > 12 and CS.out.vEgo < 15.0:           
+          accel = op_accel * 3.8
+        else:
+          accel = op_accel * interp(clu11_speed, [0., 30., 38., 50., 51., 60., 100.], [2.3, 3.4, 3.2, 1.7, 1.65, 1.4, 1.0])
 
     if accel > 0.:
       accel *= self.accel_gain * interp(clu11_speed, [35., 60., 100.], [1.5, 1.25, 1.2])
     else:
-      accel *= self.decel_gain * 1.8
+      accel *= self.decel_gain * interp(clu11_speed, [70., 75.], [1.79285, 1.8])
 
     return clip(accel, -LIMIT_DECEL, LIMIT_ACCEL), override_acc
 
