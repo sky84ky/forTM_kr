afb1afbd9a03d3f766e66479ad88fffd0f11cfce
stonerains <indawoom@naver.com>
2021-04-02 13:58:32 +0900

    수정

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -218,51 +218,51 @@ QWidget * community_panel() {
   QVBoxLayout *toggles_list = new QVBoxLayout();
   //toggles_list->setMargin(50);
 
-  toggles_list->addWidget(new ParamControl("롱컨트롤 사용",
-                                            "N 롱컨트롤 기능 사용",
-                                            "오픈파일럿이 속도를 조절합니다. 주의 하시길 바랍니다.",
+  toggles_list->addWidget(new ParamControl("LongControlEnabled",
+                                            "롱컨트롤 사용",
+                                            "N 롱컨트롤 기능 사용. 오픈파일럿이 속도를 조절합니다. 주의 하시길 바랍니다.",
                                             "../assets/offroad/icon_road.png"
                                               ));
   toggles_list->addWidget(horizontal_line());
-  toggles_list->addWidget(new ParamControl("매드모드 사용",
-                                            "HKG 매드모드 사용",
-                                            "가감속의 사용 하지 않아도 핸들 조향을 사용합니다.",
+  toggles_list->addWidget(new ParamControl("MadModeEnabled",
+                                            "매드모드 사용",
+                                            "HKG 매드모드 사용. 가감속의 사용 하지 않아도 핸들 조향을 사용합니다.",
                                             "../assets/offroad/icon_openpilot.png"
                                               ));
   toggles_list->addWidget(horizontal_line());
-  toggles_list->addWidget(new ParamControl("자동 차선변경 사용",
-                                            "자동 차선 변경",
-                                            "사용에 주의 하십시오",
+  toggles_list->addWidget(new ParamControl("AutoLaneChangeEnabled",
+                                            "자동 차선변경 사용",
+                                            "자동 차선 변경. 사용에 주의 하십시오",
                                             "../assets/offroad/icon_road.png"
                                               ));
   toggles_list->addWidget(horizontal_line());
-  toggles_list->addWidget(new ParamControl("가감속 스무서 사용",
-                                            "N Smoother",
-                                            "순정 ASCC 기능을 이용하여 가속과 감속을 부드럽게 할 수 있도록 도와 줍니다. 사용법을 정확히 인지하고 사용하십시오.",
+  toggles_list->addWidget(new ParamControl("SccSmootherEnabled",
+                                            "가감속 스무서 사용",
+                                            "N Smoother. 순정 ASCC 기능을 이용하여 가속과 감속을 부드럽게 할 수 있도록 도와 줍니다. 사용법을 정확히 인지하고 사용하십시오.",
                                             "../assets/offroad/icon_road.png"
                                             ));
   toggles_list->addWidget(horizontal_line());
-  toggles_list->addWidget(new ParamControl("커브 감속 사용",
-                                            "곡률에 따른 속도 감속 기능을 사용",
-                                            "",
+  toggles_list->addWidget(new ParamControl("SccSmootherSlowOnCurves",
+                                            "커브 감속 사용",
+                                            "SCC 설정 시 곡률에 따른 속도 감속 기능을 사용",
                                             "../assets/offroad/icon_road.png"
                                             ));
   toggles_list->addWidget(horizontal_line());
-  toggles_list->addWidget(new ParamControl("크루즈 속도의 동기화",
+  toggles_list->addWidget(new ParamControl("SccSmootherSyncGasPressed",
+                                            "크루즈 속도의 동기화",
                                             "크루즈 속도를 설정 후 엑셀로 인해 설정 속도보다 가속 속도가 높아지면 그 속도에 크루즈 설정 속도가 동기화 됩니다.",
-                                            "",
                                             "../assets/offroad/icon_road.png"
                                             ));
   toggles_list->addWidget(horizontal_line());
-  toggles_list->addWidget(new ParamControl("스무스 설정 버튼을 거리설정 버튼으로 변경",
+  toggles_list->addWidget(new ParamControl("SccSmootherSwitchGapOnly",
+                                            "스무스 설정 버튼 변경",
                                             "크루즈 갭 설정 버튼으로 순정 ASCC와 가감속 스무서 설정을 변경합니다.",
-                                            "",
                                             "../assets/offroad/icon_road.png"
                                             ));
                                             toggles_list->addWidget(horizontal_line());
-  toggles_list->addWidget(new ParamControl("디버그 내용 보기",
+  toggles_list->addWidget(new ParamControl("ShowDebugUI",
+                                            "디버그 내용 보기",
                                             "가감속 등 디버그 내용을 화면에 띄웁니다.",
-                                            "",
                                             "../assets/offroad/icon_shell.png"
                                             ));
 
