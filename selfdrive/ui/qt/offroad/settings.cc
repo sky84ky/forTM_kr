682dad42614ab080f24402b57446a8b5ee088bc2
stonerains <indawoom@naver.com>
2021-04-02 16:07:22 +0900

    Update settings.cc
    캘리브레이션 버튼

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -81,6 +81,21 @@ DevicePanel::DevicePanel(QWidget* parent) : QWidget(parent) {
   QString serial = QString::fromStdString(params.get("HardwareSerial", false));
   device_layout->addWidget(new LabelControl("시리얼", serial));
 
+  QHBoxLayout *reset_layout = new QHBoxLayout();
+  reset_layout->setSpacing(30);
+
+  // reset calibration button
+  QPushButton *reset_calib_btn = new QPushButton("캘리브레이션 리셋");
+  reset_layout->addWidget(reset_calib_btn);
+  QObject::connect(reset_calib_btn, &QPushButton::released, [=]() {
+    if (ConfirmationDialog::confirm("캘리브레이션을 다시 하시겠습니까?")) {
+      Params().remove("CalibrationParams");
+    }
+  });
+
+  device_layout->addWidget(horizontal_line());
+  device_layout->addLayout(reset_layout);
+
   // offroad-only buttons
   QList<ButtonControl*> offroad_btns;
 
