8977063b7840577a210c3f126e3ad4b248be9ea2
stonerains <indawoom@naver.com>
2021-04-02 12:46:15 +0900

    수정

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -318,7 +318,7 @@ SettingsWindow::SettingsWindow(QWidget *parent) : QFrame(parent) {
         color: grey;
         border: none;
         background: none;
-        font-size: 60px;
+        font-size: 57px;
         font-weight: 500;
         padding-top: 35px;
         padding-bottom: 35px;
