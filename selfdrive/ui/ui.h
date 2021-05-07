e3a54277e267f75a1eb84794115fac06a9587b37
stonerains <indawoom@naver.com>
2021-05-07 11:51:59 +0900

    오류수정

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -38,11 +38,9 @@
 #include "common/glutil.h"
 #include "common/util.h"
 #include "common/transformations/orientation.hpp"
-#include "messaging.hpp"
+#include "messaging.h"
 #include "visionipc.h"
 #include "visionipc_client.h"
-
-#include "qt/sound.h"
 
 #include <QObject>
 #include <QTimer>
@@ -111,7 +109,7 @@ typedef enum UIStatus {
 } UIStatus;
 
 static std::map<UIStatus, NVGcolor> bg_colors = {
-//  {STATUS_OFFROAD, nvgRGBA(0x36, 0x36, 0x36, 0xff)},
+  {STATUS_OFFROAD, nvgRGBA(0x36, 0x36, 0x36, 0xff)},
   {STATUS_DISENGAGED, nvgRGBA(0x17, 0x33, 0x49, 0x64)},
   {STATUS_ENGAGED, nvgRGBA(0x00, 0xAA, 0xFF, 0x64)},
   //{STATUS_ENGAGED, nvgRGBA(0x02, 0x70, 0xB9, 0xf1)},
@@ -141,12 +139,6 @@ typedef struct UIScene {
 //깜박이 추가 종료  
   bool is_rhd;
   bool driver_view;
-
-  std::string alert_text1;
-  std::string alert_text2;
-  std::string alert_type;
-  float alert_blinking_rate;
-  cereal::ControlsState::AlertSize alert_size;
 
   cereal::PandaState::PandaType pandaType;
   NetStatus athenaStatus;
@@ -201,7 +193,6 @@ typedef struct UIState {
 
   std::unique_ptr<SubMaster> sm;
 
-  std::unique_ptr<Sound> sound;
   UIStatus status;
   UIScene scene;
 
